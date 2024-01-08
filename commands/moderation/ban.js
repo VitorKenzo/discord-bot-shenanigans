6e6e288442c21537c84eb672d64cc6e409de4196
Vitor Kenzo F. Pellegatti <vkenzofp@gmail.com>
2024-01-08 12:24:25 -0300

    Ban command almost working

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { Interaction, SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
     category: 'moderation',
@@ -14,16 +14,52 @@ module.exports = {
             option.setName('reason')
                 .setDescription('Reasons for the ban')
                 .setRequired(true))
-        .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers | PermissionFlagsBits.KickMembers)
+        .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
         .setDMPermission(false),
+    /**
+     * 
+     * @param {Interaction} interaction 
+     */
     async execute(interaction) {
         
         const user = interaction.options.getUser('person');
         const reason = interaction.options.getString('reason');
 
-        console.log(user);
-        console.log(reason);
+        await interaction.deferReply();
 
-        interaction.reply('Banning...');
+        const target = await interaction.guild.members.fetch(user);
+
+        if(!target) {
+            await interaction.editReply("User does not exist in the server anymore");
+            return;
+        }
+
+        if(target.id === interaction.guild.ownerId) {
+            await interaction.editReply("You cannot ban the owner of the server");
+            return;
+        }
+
+        const targetUserRole = target.roles.highest.position;
+        const requestUserRole = interaction.member.roles.highest.position;
+        const botRole = interaction.guild.members.me.roles.highest.position;
+
+        if(targetUserRole > requestUserRole) {
+            await interaction.editReply('Cannot ban user because they have a higher role than you');
+            return;
+        }
+
+        if(targetUserRole > botRole) {
+            await interaction.editReply('Cannot ban user because they have a higher role than me (bot)');
+            return;
+        }
+
+        // banning the user
+        try {
+            await interaction.guild.members.ban(user);
+            await interaction.editReply(`User ${target} was banned!\nReason: ${reason}`);
+        } catch (error) {
+            console.log(`Error while banning the user: ${error}`);
+        }
+
     }           
 }
