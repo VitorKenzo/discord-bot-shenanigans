3537031680c0a9deb742d606389fd328381f628f
Vitor Kenzo F. Pellegatti <vkenzofp@gmail.com>
2024-01-08 15:46:13 -0300

    Role giving command functional

--- commands/moderation/role.js
+++ commands/moderation/role.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { Interaction, SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 // to be able to get the string time sent and transform it in miliseconds
 const ms = require('ms');
 
@@ -21,16 +21,52 @@ module.exports = {
                 .setDescription('Duration the role will be applied (30m, 1h, 1 day)')
                 .setRequired(true))
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+
+    /**
+     * 
+     * @param {Interaction} interaction 
+     */
     async execute(interaction) {
         
         const user = interaction.options.getUser('user');
-        const reason = interaction.options.getRole('role');
+        const role = interaction.options.getRole('role');
         const duration = interaction.options.getString('duration');
         
-        console.log(user);
-        console.log(reason);
-        console.log(duration);
-        
-        interaction.reply('Giving role...')
+        await interaction.deferReply();
+
+        const target = await interaction.guild.members.fetch(user);
+
+        if(!target) {
+            await interaction.editReply('The user does not exist in the server');
+            return;
+        }
+
+        const msDuration = ms(duration);
+        if (isNaN(msDuration)) {
+            await interaction.editReply('Provide a valid timeout duration');
+            return;
+        }
+
+        // giving role for the determined time
+        try {
+
+            // just to reply with a pretty time
+            const { default: prettyMS } = await import('pretty-ms');
+
+            if(target.roles.cache.some(r => r.name === role.name)){
+                interaction.editReply(`User ${target} already has the role ${role}`);
+                return;
+            }
+
+            target.roles.add(role);
+            interaction.editReply(`${target} is ${role} for ${prettyMS(msDuration)}`);
+
+            setTimeout(async () => {
+                await target.roles.remove(role);
+            }, msDuration);
+
+        } catch (error) {
+            console.log(`Error happened while giving the role ${error}`);
+        }
     }           
 }
