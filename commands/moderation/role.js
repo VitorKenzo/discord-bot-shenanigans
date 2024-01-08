4922d3997fc0651fcc6d4bcaa0f46eac421dd1dc
Vitor Kenzo F. Pellegatti <vkenzofp@gmail.com>
2024-01-08 12:39:21 -0300

    Changing a bit the structure of the role command

--- commands/moderation/role.js
+++ commands/moderation/role.js
@@ -1,4 +1,6 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+// to be able to get the string time sent and transform it in miliseconds
+const ms = require('ms');
 
 module.exports = {
     category: 'moderation',
@@ -10,7 +12,7 @@ module.exports = {
             option.setName('user')
                 .setDescription('User to receive the role')
                 .setRequired(true))
-        .addStringOption(option =>
+        .addRoleOption(option =>
             option.setName('role')
                 .setDescription('Role to be given')
                 .setRequired(true))
@@ -18,13 +20,12 @@ module.exports = {
             option.setName('duration')
                 .setDescription('Duration the role will be applied (30m, 1h, 1 day)')
                 .setRequired(true))
-        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
-        .setDMPermission(false),
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
     async execute(interaction) {
         
-        const user = interaction.options('user');
-        const reason = interaction.options.getString('role');
-        const duration = interaction.options.getInteger('duration')
+        const user = interaction.options.getUser('user');
+        const reason = interaction.options.getRole('role');
+        const duration = interaction.options.getString('duration');
         
         console.log(user);
         console.log(reason);
